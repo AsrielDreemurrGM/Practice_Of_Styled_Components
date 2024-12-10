99156c27a87a0a43bfcab3f372f6b3f869a3fb37
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-12-10 00:18:32 -0300

    Convertido o Hero Para Styled Components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { Formulario, ContainerDoTitulo, TituloDoHero } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <Formulario>
+    <ContainerDoTitulo>
+      <TituloDoHero>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </TituloDoHero>
+    </ContainerDoTitulo>
+  </Formulario>
 )
 
 export default Hero
