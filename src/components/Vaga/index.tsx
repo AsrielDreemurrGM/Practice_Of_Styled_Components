8af7534ab5755173f8c621d365b73bc0b0907009
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-12-10 13:45:34 -0300

    Convertido a Vaga Para Styled Components

--- src/components/Vaga/index.tsx
+++ src/components/Vaga/index.tsx
@@ -1,4 +1,4 @@
-import styles from './Vaga.module.css'
+import { ItemDaVaga, TituloDaVaga, LinkDaVaga } from './styles'
 
 type Props = {
   titulo: string
@@ -11,8 +11,8 @@ type Props = {
 }
 
 const Vaga = (props: Props) => (
-  <li className={styles.vaga}>
-    <h3 className={styles.vagaTitulo}>{props.titulo}</h3>
+  <ItemDaVaga>
+    <TituloDaVaga>{props.titulo}</TituloDaVaga>
     <ul>
       <li>Localizacao: {props.localizacao}</li>
       <li>Senioridade: {props.nivel}</li>
@@ -22,10 +22,8 @@ const Vaga = (props: Props) => (
       </li>
       <li>Requisitos: {props.requisitos.join(', ')}</li>
     </ul>
-    <a className={styles.vagaLink} href="#">
-      Ver detalhes e candidatar-se
-    </a>
-  </li>
+    <LinkDaVaga href="#">Ver detalhes e candidatar-se</LinkDaVaga>
+  </ItemDaVaga>
 )
 
 export default Vaga
